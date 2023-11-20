5fe1af38c2f9c35a5964924ae64d0d86559b2a46
Leonardo Albano <leonardoalbanom@yahoo.com.br>
2023-11-20 09:22:33 -0300

    first commit

---
+++ src/controllers/NotesController.js
@@ -0,0 +1,65 @@
+// Importa o módulo 'knex' para realizar operações no banco de dados.
+const knex = require("../database/knex");
+
+// Cria uma classe chamada NotesController para lidar com operações relacionadas a notas.
+class NotesController {
+    // Método assíncrono para criar uma nova nota.
+    async create(request, response) {
+        // Extrai informações do corpo da requisição.
+        const { title, description, tags, links } = request.body;
+
+        // Extrai o ID do usuário da requisição.
+        const { user_id } = request.body;
+
+        // Insere uma nova nota no banco de dados e obtém o ID da nota criada.
+        const [ note_id ] = await knex("notes").insert({
+            title,
+            description,
+            user_id
+        });
+
+        // Cria um array de objetos para inserir os links no banco de dados, associando-os à nota criada.
+        const linksInsert = links.map(link => {
+            return {
+                note_id,
+                url: link
+            };
+        });
+
+        // Insere os links associados à nota no banco de dados.
+        await knex("links").insert(linksInsert);
+
+        // Cria um array de objetos para inserir as tags no banco de dados, associando-as à nota criada.
+        const tagsInsert = tags.map(name => {
+            return {
+                note_id,
+                name,
+                user_id
+            };
+        });
+
+        // Insere as tags associadas à nota no banco de dados.
+        await knex("tags").insert(tagsInsert);
+
+        // Responde à requisição com uma resposta JSON vazia.
+        response.json();
+    }
+
+// Método assíncrono chamado "show" que recebe uma requisição e uma resposta.
+async show(request, response) {
+    // Extrai o parâmetro "id" da URL da requisição.
+    const { id } = request.params;
+  
+    // Utiliza o Knex para realizar uma consulta à tabela "notes" no banco de dados.
+    // O método "where" filtra os registros onde a coluna "id" é igual ao parâmetro extraído da URL.
+    // O método "first" retorna apenas o primeiro resultado da consulta.
+    const note = await knex("notes").where({ id }).first();
+  
+    // Retorna uma resposta JSON contendo o resultado da consulta.
+    return response.json(note);
+  }
+  
+}
+
+// Exporta a classe NotesController para ser utilizada em outros arquivos.
+module.exports = NotesController;
